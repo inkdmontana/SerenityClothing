22e7e7ee45a48df4ca87d6d53c1a9f578091d3a5
inkdmontana <143229256+inkdmontana@users.noreply.github.com>
2024-07-09 11:18:21 -0400

    Update database.py

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="PW123!",
+        password="abc123",
         database="serenity_clothing"
     )
 
